0e06ca57584c0c62af23d5eb684123972f433e4f
mamad67M <mam.diallo163@gmail.com>
2020-10-07 23:43:04 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo3POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complexe c = new Complexe();
+            Console.ReadLine();
+        }
+    }
+}
